74880b0bfd81928ab9e08a62d7d7aef7702ff56a
Юрасов Вячеслав <2665603@gmail.com>
2020-12-17 13:31:02 +0500

    Initial commit

---
+++ Commands.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+
+namespace MyNotepad {
+    class Commands {
+        static Commands() {
+            Exit = new RoutedCommand("Exit", typeof(MainWindow));
+            New = new RoutedCommand("New", typeof(MainWindow));
+            Open = new RoutedCommand("Open", typeof(MainWindow));
+            Save = new RoutedCommand("Save", typeof(MainWindow));
+        }
+        public static RoutedCommand Exit { get; set; }
+        public static RoutedCommand New { get; set; }
+        public static RoutedCommand Open { get; set; }
+        public static RoutedCommand Save { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyNotepad {
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Exit_Click(object sender, RoutedEventArgs e) {
+            var response = MessageBox.Show("Do you really want to exit?", "Exiting...", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+            if (response == MessageBoxResult.No) return;
+             else Application.Current.Shutdown();
+        }
+
+        private void MenuItem_New_Click(object sender, RoutedEventArgs e) {
+            MessageBox.Show("New");
+        }
+
+        private void MenuItem_Open_Click(object sender, RoutedEventArgs e) {
+            MessageBox.Show("Open");
+        }
+
+        private void MenuItem_Save_Click(object sender, RoutedEventArgs e) {
+            MessageBox.Show("Save");
+        }
+    }
+}
